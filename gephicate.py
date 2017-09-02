1e37faa6ad5ebb9953e4a911a998a69edfa0ff21
Ted Dawson <dawson.ted@gmail.com>
2017-09-01 20:48:11 -0500

    now turns protocol into lists of 3 words surrounding action

--- gephicate.py
+++ gephicate.py
@@ -10,15 +10,16 @@ read_start = whole_text.find('<begin>')
 read_end = whole_text.find('<end>')
 text = whole_text[read_start: read_end]
 
-z = len(action) + 1
-
 g = open(target, 'a')
 
 text_list = text.split()
 
+i = 0
+
 for token in text_list:
     if token == action:
-	    g.write('\n' + token)
+	   g.write('\n' + str(text_list[i-1: i+2]))
+    i += 1
 
 f.close()
 g.close()
@@ -26,7 +27,8 @@ g.close()
 
 # retired idea for finding words in plain text (not list)
 
-'''i = 0
+'''z = len(action) + 1
+i = 0
 j = len(text)
 while i < j:
     wordspot = text.find(action)
