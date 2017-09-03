3ed6d4c07eee7412aaf0bdd5bb9fb762911023b8
Ted Dawson <dawson.ted@gmail.com>
2017-09-02 19:08:06 -0500

    now produces list of pairs, nodes csv

--- gephicate.py
+++ gephicate.py
@@ -1,8 +1,9 @@
-# python module for yanking items from a film protocol into a gephi-ready csv
+# python script for yanking items from a film protocol into a gephi-ready csv
 
 from sys import argv
+import csv
 
-script, source, target, action = argv
+script, source, action = argv
 
 f = open(source, 'r')
 whole_text = f.read()
@@ -10,36 +11,46 @@ read_start = whole_text.find('<begin>')
 read_end = whole_text.find('<end>')
 text = whole_text[read_start: read_end]
 
-g = open(target, 'a')
-
+# create a file to show which items have been taken from protocol
+g = open('list-of-relations.txt', 'a')
+g.write('This list shows the relations from ' + source + ' that Gephicate used to construct the nodes and edges CSVs.')
 text_list = text.split()
 
+# move through text and extract words on either side of action, putting these both in g and in a list for further use
 i = 0
-
+pairs = []
 for token in text_list:
     if token == action:
 	   g.write('\n' + str(text_list[i-1: i+2]))
+	   pairs += [text_list[i-1], text_list[i+1]]
     i += 1
 
+# close protocol source file, as all necessary info is removed
 f.close()
 g.close()
 
-
-# retired idea for finding words in plain text (not list)
-
-'''z = len(action) + 1
-i = 0
-j = len(text)
-while i < j:
-    wordspot = text.find(action)
-    if wordspot == -1:
-        break
+print pairs
+# extract nodes from pairs
+nodes = []
+for item in pairs:
+    if item in nodes:
+	    pass
     else:
-        print text[wordspot + z]
-        texto = text[wordspot + z: ]
-        i = i + (len(text) - len(texto))
-        text = texto'''
-	
-# print text
+	    nodes.append(item)
+
+# associate nodes with numbers
+numbered_nodes = list(enumerate(nodes, 1))
+
+# create nodes.csv and populate with numbered_nodes values
+with open('nodes.csv', 'w') as csvfile:
+    fieldnames = ['id', 'label']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for x,y in numbered_nodes:
+        writer.writerow({'id': x,'label': y})
+
+# next steps: replace names in pairs with node ids, create edges csv
+
 
 
